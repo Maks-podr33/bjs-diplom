bc7b79895d1169204a299a0f16f3b6bbc8c644f4
maks_podr33 <mixx33@yandex.ru>
2024-10-20 23:10:24 +0300

    Дипломное задание к курсу «Основы JavaScript». Готово на проверку - 2

--- public/homePage.js
+++ public/homePage.js
@@ -26,16 +26,29 @@ function fillRatesBoard() {
 	});
 }
 fillRatesBoard();
-setInterval(() => fillRatesBoard(), 60000);
+setInterval(fillRatesBoard, 60000);  // <--Это, вместо этого-->  setInterval(() => fillRatesBoard(), 60000);
 
 const moneyManager = new MoneyManager();
 moneyManager.addMoneyCallback = moneyManager.addMoneyAction = data => {
 	ApiConnector.addMoney(data, response => {
-		if (response.success == true) {
+		let amount = data.amount;
+		let currency = data.currency;
+		// console.log(amount, currency);
+		console.log(response);
+		// (response.success == true) 
+		if (!amount && !currency) {
+			moneyManager.setMessage(false, "Укажите сумму и валюту пополнения!");
+		} else if (!currency) {
+			moneyManager.setMessage(false, "Не указана валюта для пополнения!");
+		} else if (!amount) {
+			moneyManager.setMessage(false, "Не указана сумма для пополнения!");
+		} else if (amount <= 0) {
+			moneyManager.setMessage(false, "Сумма пополнения должна быть больше НУЛЯ! Попробуйте ещё раз.");
+		} else if (response.success == true) { //
 			moneyManager.setMessage(true, "Счёт успешно пополнен");
 			ProfileWidget.showProfile(response.data);
 		} else {
-			moneyManager.setMessage(false, "Ошибка при пополнении счёта, счёт не пополнен");
+			moneyManager.setMessage(false, "Ошибка при пополнении! Попробуйте ещё раз.");
 		}
 	});
 }
@@ -46,7 +59,7 @@ moneyManager.conversionMoneyCallback = moneyManager.conversionMoneyAction = data
 			moneyManager.setMessage(true, "Конвертация успешно исполнена");
 			ProfileWidget.showProfile(response.data);
 		} else {
-			moneyManager.setMessage(false, "Ошибка при Конвертации, попробуйте ещё раз");
+			moneyManager.setMessage(false, response.error);
 		}
 	});
 }
@@ -57,7 +70,7 @@ moneyManager.sendMoneyCallback = moneyManager.sendMoneyAction = data => {
 			moneyManager.setMessage(true, "Перевод выполнен успешно");
 			ProfileWidget.showProfile(response.data);
 		} else {
-			moneyManager.setMessage(false, "Ошибка перевода средств, попробуйте ещё раз");
+			moneyManager.setMessage(false, response.error);
 		}
 	});
 }
@@ -84,7 +97,7 @@ favoritesWidget.addUserCallback = FavoritesWidget.getData = data => {
 			fillFavoritesTable(response);
 			favoritesWidget.setMessage(true, "Пользователь успешно добавлен");
 		} else {
-			favoritesWidget.setMessage(false, "Ошибка при добавлении пользователя, попробуйте ещё раз");
+			favoritesWidget.setMessage(false, response.error);
 		}
 	});
 }
@@ -95,7 +108,7 @@ favoritesWidget.removeUserCallback = FavoritesWidget.getData = data => {
 			fillFavoritesTable(response);
 			favoritesWidget.setMessage(true, "Пользователь успешно удален");
 		} else {
-			favoritesWidget.setMessage(false, "Ошибка при удалении пользователя, попробуйте ещё раз");
+			favoritesWidget.setMessage(false, response.error);
 		}
 	});
 }
